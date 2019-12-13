4965e1f6c7bce03e0cfa87e2a49eac4b2008b763
Michael Kirsch <michaelskirsch@gmail.com>
2019-12-13 12:25:15 -0700

    Ophidian bank

--- queue.js
+++ queue.js
@@ -1,60 +1,59 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 class Queue {
-    constructor() {
-        this.first = null;
-        this.last = null;
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new Node(data);
+
+    if (this.first === null) {
+      this.first = node;
     }
 
-    enqueue(data) {
-        const node = new Node(data);
-
-        if (this.first === null) {
-            this.first = node;
-        }
-
-        if (this.last) {
-            this.last.next = node;
-        }
-        this.last = node;
+    if (this.last) {
+      this.last.next = node;
     }
+    this.last = node;
+  }
 
-    dequeue() {
-        if (this.first === null) {
-            return;
-        }
-        const node = this.first;
-        this.first = this.first.next;
-        if (node === this.last) {
-            this.last = null;
-        }
-        return node.value;
+  dequeue() {
+    if (this.first === null) {
+      return null;
     }
-    remove(data) {
-        if (!this.first) {
-            return null;
-        }
-        if (this.first.value === data) {
-            this.first = this.first.next;
-            return;
-        }
-        let currNode = this.first;
-        let previousNode = this.first;
-
-        while ((currNode !== null) && (currNode.value !== data)) {
-            previousNode = currNode;
-            currNode = currNode.next;
-        }
-        if (currNode === null) {
-            console.log('Item not found');
-            return;
-        }
-        previousNode.next = currNode.next;
+    const node = this.first;
+    this.first = this.first.next;
+    if (node === this.last) {
+      this.last = null;
     }
+    return node.value;
+  }
+  remove(data) {
+    if (!this.first) {
+      return null;
+    }
+    if (this.first.value === data) {
+      this.first = this.first.next;
+      return;
+    }
+    let currNode = this.first;
+    let previousNode = this.first;
 
+    while (currNode !== null && currNode.value !== data) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
 }
 module.exports = Queue;
--- queueMain.js
+++ queueMain.js
@@ -1,57 +1,72 @@
 const Queue = require('./queue');
 const QueueDLL = require('./queuedll.js');
 
-function peek(queue){
-    if(!isEmpty(queue))
-    return queue.first.value;
-    else
-        return null;
+function peek(queue) {
+  if (!isEmpty(queue)) return queue.first.value;
+  else return null;
 }
-function isEmpty(queue){
-    return queue.first === null;
+function isEmpty(queue) {
+  return queue.first === null;
 }
-function display(queue){
-    const temp = new Queue();
-    if (isEmpty(queue)) return null;
-    let item = queue.dequeue();
-    let string = '[';
+function display(queue) {
+  const temp = new Queue();
+  if (isEmpty(queue)) return null;
+  let item = queue.dequeue();
+  let string = '[';
 
-    while (item !==  null && item !== undefined) {
-        string += `${item}, `;
-        temp.enqueue(item);
-        item = queue.dequeue();
-    }
+  while (item !== null && item !== undefined) {
+    string += `${item}, `;
+    temp.enqueue(item);
+    item = queue.dequeue();
+  }
+  item = temp.dequeue();
+  while (item !== null && item !== undefined) {
+    queue.enqueue(item);
     item = temp.dequeue();
-    while (item !== null && item !== undefined) {
-        queue.enqueue(item);
-        item = temp.dequeue();
+  }
+  return string.substring(0, string.length - 2) + ']';
+}
+
+function squareDanceParing() {}
+
+function Ophidian(queue) {
+  let customer = queue.dequeue();
+  while (customer !== null) {
+    if (Math.random() < 0.25) {
+      console.log(`Sending ${customer} back to the end of the line`);
+      queue.enqueue(customer);
+    } else {
+      console.log(`Helping ${customer}`);
     }
-    return string.substring(0, string.length-2)+']';
+    customer = queue.dequeue();
+  }
 }
 
-function squareDanceParing(){
-}
+function main() {
+  let starTrekQ = new Queue();
 
+  starTrekQ.enqueue('Kirk');
+  starTrekQ.enqueue('Spock');
+  starTrekQ.enqueue('Uhura');
+  starTrekQ.enqueue('Sulu');
+  starTrekQ.enqueue('Checkov');
+  starTrekQ.remove('Spock');
+  console.log(display(starTrekQ));
 
-function main(){
-    let starTrekQ = new Queue();
+  let starTrekQ2 = new QueueDLL();
+  starTrekQ2.enqueue('Kirk');
+  starTrekQ2.enqueue('Spock');
+  starTrekQ2.enqueue('Uhura');
+  starTrekQ2.enqueue('Sulu');
+  starTrekQ2.enqueue('Checkov');
+  starTrekQ2.remove('Spock');
+  console.log(starTrekQ2.tail.value, starTrekQ2.head.value);
+  console.log(display(starTrekQ2));
 
-    starTrekQ.enqueue('Kirk');
-    starTrekQ.enqueue('Spock');
-    starTrekQ.enqueue('Uhura');
-    starTrekQ.enqueue('Sulu');
-    starTrekQ.enqueue('Checkov');
-    starTrekQ.remove('Spock');
-    console.log(display(starTrekQ));
-
-    let starTrekQ2 = new QueueDLL();
-    starTrekQ2.enqueue('Kirk');
-    starTrekQ2.enqueue('Spock');
-    starTrekQ2.enqueue('Uhura');
-    starTrekQ2.enqueue('Sulu');
-    starTrekQ2.enqueue('Checkov');
-    starTrekQ2.remove('Spock');
-    console.log(starTrekQ2.tail.value, starTrekQ2.head.value)
-    console.log(display(starTrekQ2));
+  let ophidianQueue = new Queue();
+  for (let i = 0; i < 20; i++) {
+    ophidianQueue.enqueue(i);
+  }
+  Ophidian(ophidianQueue);
 }
 main();
