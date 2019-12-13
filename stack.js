bda3aecf1709e6b7b69931103c037d42af5583b0
Michael Kirsch <michaelskirsch@gmail.com>
2019-12-13 09:23:45 -0700

    Stack class

---
+++ stack.js
@@ -0,0 +1,23 @@
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+  push(data) {
+    this.top = new _Node(data, this.top);
+  }
+  pop() {
+    const temp = this.top;
+    if (temp === null) return null;
+    this.top = temp.next;
+    return temp;
+  }
+}
+
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+module.exports = Stack;
